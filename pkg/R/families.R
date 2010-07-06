8444f2b4bdc5c4aca3bac82752d6c07868320562
hofner <hofner@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-06 15:59:23 +0000

    familes changed (defaults, usage of offset values, ...) documentation added
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/gamboostlss@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/families.R
+++ pkg/R/families.R
@@ -1,8 +1,9 @@
 ###
 # Negative Binomial LSS Family
 
-NBinomialMu <- function(off = list(sigma = 1)) {
-    sigma <- off[["sigma"]]
+NBinomialMu <- function(mu = NULL, sigma = NULL) {
+    offset <- function(y, w)
+        log(mu)
     loss <- function(sigma, y, f)
         -(lgamma(y + sigma) - lgamma(sigma) - lgamma(y + 1) + sigma * log(sigma)
           - sigma * log(exp(f) + sigma) + y * f - y * log(exp(f) + sigma))
@@ -14,22 +15,22 @@ NBinomialMu <- function(off = list(sigma = 1)) {
         RET <- y - (y + sigma)/(exp(f) + sigma) * exp(f)
         return(RET)
     }
-    Family(ngradient = ngradient, risk = risk, loss = loss,
+    Family(ngradient = ngradient, offset = offset, risk = risk, loss = loss,
            response = function(f) exp(f),
            check_y = function(y) {
                stopifnot(all.equal(unique(y - floor(y)), 0))
                y
-           }, name = "Negative Negative-Binomial Likelihood (mu)")
+           }, name = "Negative Negative-Binomial Likelihood: mu (log link)")
 }
 
-NBinomialSigma <- function(off = list(mu = 0)) {
+NBinomialSigma <- function(mu = NULL, sigma = NULL) {
     # this family boosts sigma therefore f is sigma
-    mu <- off[["mu"]]
     loss <- function(mu, y, f)
         -(lgamma(y + exp(f)) - lgamma(exp(f)) - lgamma(y + 1) + exp(f) * f -
           exp(f) * log(mu + exp(f)) + y * log(mu) - y * log(mu + exp(f)))
 
-    offset <- function(y,w) 0
+    offset <- function(y,w)
+        log(sigma)
     risk <- function(y, f, w = 1){
         RET <- sum(w * loss(y = y, f = f, mu= mu))
         return(RET)
@@ -39,18 +40,20 @@ NBinomialSigma <- function(off = list(mu = 0)) {
                        log(mu +exp(f)) - (exp(f) + y)/(mu +exp(f)))
         return(RET)
     }
-    Family(ngradient = ngradient,offset=offset, risk = risk, loss = loss,
+    Family(ngradient = ngradient, offset = offset, risk = risk, loss = loss,
            response = function(f) exp(f),
            check_y = function(y) {
                stopifnot(all.equal(unique(y - floor(y)), 0))
                y
-           }, name = "Negative Negative-Binomial Likelihood (sigma)")
+           }, name = "Negative Negative-Binomial Likelihood: sigma (log link)")
 }
 
 
-NBinomialLSS <- function(mu = 0, sigma = 1){
-    RET <- list(mu = NBinomialMu(off = list(sigma = sigma)),
-                sigma = NBinomialSigma(off = list(mu = mu)))
+NBinomialLSS <- function(mu = 1, sigma = 1){
+    if (sigma <= 0)
+        stop(sQuote("sigma"), " must be greater than zero")
+    RET <- list(mu = NBinomialMu(mu = mu, sigma = sigma),
+                sigma = NBinomialSigma(mu = mu, sigma = sigma))
     class(RET) <- "families"
     return(RET)
 }
@@ -60,16 +63,14 @@ NBinomialLSS <- function(mu = 0, sigma = 1){
 ###
 # T-distribution LSS Family
 
-StudentTMu <- function(off = list(df=1, sigma=1)) {
-    df <- off[["df"]]
-    sigma <- off[["sigma"]]
-
+StudentTMu <- function(mu = NULL, sigma = NULL, df = NULL) {
     loss <- function(df, sigma,y, f){
         -1 * (lgamma((df+1)/2) - log(sigma) - lgamma(1/2) - lgamma(df/2) - 0.5 *
               log(df) - (df+1)/2 * log(1 + (y-f)^2 / (df * sigma^2)))
     }
 
-    offset <- function(y,w) 0
+    offset <- function(y,w)
+        mu
     risk <- function(y, f, w = 1){
         sum(w * loss(y = y, f = f, df = df, sigma = sigma))
     }
@@ -87,16 +88,14 @@ StudentTMu <- function(off = list(df=1, sigma=1)) {
            },  name = "Student's t-distribution: mu (id link)")
 }
 
-StudentTSigma <- function(off = list(df=1, mu=0)) {
-    mu <- off[["mu"]]
-    df <- off[["df"]]
-
+StudentTSigma <- function(mu = NULL, sigma = NULL, df = NULL) {
     loss <- function(df, mu, y, f){
         -1 * (lgamma((df+1)/2) - f - lgamma(1/2) - lgamma(df/2) - 0.5 * log(df) -
               (df+1)/2 * log(1 + (y-mu)^2 / (df * exp(2 * f))))
     }
 
-    offset <- function(y,w) 1
+    offset <- function(y,w)
+        log(sigma)
     risk <- function(y, f, w = 1){
         sum(w * loss(y = y, f = f, df = df, mu = mu))
     }
@@ -114,16 +113,14 @@ StudentTSigma <- function(off = list(df=1, mu=0)) {
            },  name = "Student's t-distribution: sigma (log link)")
 }
 
-StudentTDf <- function(off = list(sigma=1, mu=0)) {
-    mu <- off[["mu"]]
-    sigma <- off[["sigma"]]
-
+StudentTDf <- function(mu = NULL, sigma = NULL, df = NULL) {
     loss <- function(sigma, mu,y, f){
         -1 * (lgamma((exp(f)+1)/2) - log(sigma) - lgamma(1/2) - lgamma(exp(f)/2) -
               0.5*f - (exp(f)+1)/2 * log(1 + (y-mu)^2 / (exp(f)*sigma^2)))
     }
 
-    offset <- function(y,w) 1
+    offset <- function(y,w)
+        log(df)
     risk <- function(y, f, w = 1){
         sum(w * loss(y = y, f = f, sigma = sigma, mu = mu))
     }
@@ -145,10 +142,13 @@ StudentTDf <- function(off = list(sigma=1, mu=0)) {
 
 
 
-StudentTLSS <- function(mu = 0, sigma=1, df=1){
-    RET <- list(mu = StudentTMu(off = list(sigma = sigma, df = df)),
-                sigma = StudentTSigma(off = list(mu = mu, df = df)),
-                df = StudentTDf(off = list(mu = mu, sigma = sigma)))
+StudentTLSS <- function(mu = 0, sigma = 1, df = 4){
+    if (sigma <= 0 || df <= 0)
+        stop(sQuote("sigma"), " and ", sQuote("df"),
+             " must be greater than zero")
+    RET <- list(mu = StudentTMu(mu = mu, sigma = sigma, df = df),
+                sigma = StudentTSigma(mu = mu, sigma = sigma, df = df),
+                df = StudentTDf(mu = mu, sigma = sigma, df = df))
     class(RET) <- "families"
     return(RET)
 }
