a5a92676b41790ae9194f21827e5908f4ecd4b3a
janek <janek.thomas@web.de>
2015-11-26 17:17:36 +0100

    test for oob fitting bug

--- pkg/tests/nc_fitting.R
+++ pkg/tests/nc_fitting.R
@@ -21,3 +21,19 @@ dat <- data.frame(x1, x2, x3, x4, x5, x6, y)
 model <- glmboostLSS(y ~ ., families = NBinomialLSS(), data = dat,
                      control = boost_control(mstop = 3), cycling = FALSE)
 
+
+set.seed(2611)
+#Regular fit and fit with risk = "oob" should be identical
+w <- sample(c(0,1), 1000, replace = TRUE)
+
+model <- glmboostLSS(y ~ ., families = NBinomialLSS(), data = dat,
+                     control = boost_control(mstop = 100), cycling = TRUE,
+                     weights = w)
+
+model_oob <- glmboostLSS(y ~ ., families = NBinomialLSS(), data = dat,
+                         control = boost_control(mstop = 100, risk = "oob"), 
+                         cycling = TRUE,
+                         weights = w)
+
+stopifnot(all(unlist(model$mu$coef()) ==  unlist(model_oob$mu$coef())) &
+          all(unlist(model$sigma$coef()) ==  unlist(model_oob$sigma$coef())))
