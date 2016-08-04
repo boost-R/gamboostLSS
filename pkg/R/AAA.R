9f50b4377aaaf1bfb24761aa8d03bdd23bbeddf5
Benjamin Hofner <benjamin.hofner@fau.de>
2016-08-04 16:41:57 +0200

    Moved patch/ to parent directory

--- pkg/R/AAA.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    ### stabilize negative gradient by using a multiplicative factor based on
-    ### the variance of the negative gradient
-    options(gamboostLSS_stab_ngrad = FALSE)
-}
--- pkg/R/gamboostLSS_intern.R
+++
@@ -1,5 +0,0 @@
-gamboostLSS_intern <- function(..., fun = c("check", "do_trace")) {
-
-    fun <- match.arg(fun)
-    do.call(fun, list(...))
-}
