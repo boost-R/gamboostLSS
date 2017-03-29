6309410fc4538f1a4bac3e15c673185bc9c78d06
Janek <janek.thomas@web.de>
2017-03-29 11:39:43 +0200

    initialize combined_risk in subset

--- R/mboostLSS.R
+++ R/mboostLSS.R
@@ -4,14 +4,14 @@
 ### (glm/gam/m/black)boostLSS functions
 
 mboostLSS <- function(formula, data = list(), families = GaussianLSS(),
-                      control = boost_control(), weights = NULL, 
+                      control = boost_control(), weights = NULL,
                       method = c("cyclic", "noncyclic"), ...){
-    
+
     cl <- match.call()
     if(is.null(cl$families))
         cl$families <- families
     method <- match.arg(method)
-    
+
     fit <- mboostLSS_fit(formula = formula, data = data, families = families,
                          control = control, weights = weights, ...,
                          fun = mboost, funchar = "mboost", call = cl,
@@ -20,47 +20,47 @@ mboostLSS <- function(formula, data = list(), families = GaussianLSS(),
 }
 
 glmboostLSS <- function(formula, data = list(), families = GaussianLSS(),
-                        control = boost_control(), weights = NULL, 
+                        control = boost_control(), weights = NULL,
                         method = c("cyclic", "noncyclic"), ...){
-    
+
     cl <- match.call()
     if(is.null(cl$families))
         cl$families <- families
     method <- match.arg(method)
-    
+
     fit <- mboostLSS_fit(formula = formula, data = data, families = families,
                          control = control, weights = weights, ...,
                          fun = glmboost, funchar = "glmboost", call = cl,
                          method = method)
-    
+
     return(fit)
 }
 
 gamboostLSS <- function(formula, data = list(), families = GaussianLSS(),
-                        control = boost_control(), weights = NULL, 
+                        control = boost_control(), weights = NULL,
                         method = c("cyclic", "noncyclic"), ...){
-    
+
     cl <- match.call()
     if(is.null(cl$families))
         cl$families <- families
     method <- match.arg(method)
-    
+
     fit <- mboostLSS_fit(formula = formula, data = data, families = families,
                          control = control, weights = weights, ...,
                          fun = gamboost, funchar = "gamboost", call = cl,
                          method = method)
-    
+
     return(fit)
 }
 
 blackboostLSS <- function(formula, data = list(), families = GaussianLSS(),
-                          control = boost_control(), weights = NULL, 
+                          control = boost_control(), weights = NULL,
                           method = c("cyclic", "noncyclic"), ...){
     cl <- match.call()
     if(is.null(cl$families))
         cl$families <- families
     method <- match.arg(method)
-    
+
     fit <- mboostLSS_fit(formula = formula, data = data, families = families,
                          control = control, weights = weights, ...,
                          fun = blackboost, funchar = "blackboost", call = cl,
@@ -74,15 +74,15 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                           control = boost_control(), weights = NULL,
                           fun = mboost, funchar = "mboost", call = NULL,
                           method, ...){
-    
+
     if (length(families) == 0)
         stop(sQuote("families"), " not specified")
-    
+
     if ("offset" %in% names(list(...)))
         stop("Do not use argument ", sQuote("offset"),
              ". Please specify offsets via families")
     ### Use mu in "families" to specify offset in mu-family etc.
-    
+
     if (is.list(formula)){
         if (!all(names(formula) %in% names(families)) ||
             length(unique(names(formula))) != length(names(families)))
@@ -105,23 +105,23 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
             tmp[[i]] <- formula
         formula <- tmp
     }
-    
+
     mstop <- mstoparg <- control$mstop
     control$mstop <- 0
     if (method == "cyclic")
         mstop <- check(mstop, "mstop", names(families))
-    
-    
+
+
     nu <- control$nu
     nu <- check(nu, "nu", names(families))
-    
+
     if (is.list(control$risk) || is.list(control$center) || is.list(control$trace))
         stop(sQuote("risk"),", ", sQuote("center"), " and ", sQuote("trace") ,
              " cannot be lists in ", sQuote("boost_control"))
-    
+
     trace <- control$trace
     control$trace <- FALSE
-    
+
     w <- weights
     if (is.null(weights)){
         if (!is.list(response)) {
@@ -131,12 +131,12 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
         }
     }
     weights <- rescale_weights(weights)
-    
+
     fit <- vector("list", length = length(families))
     names(fit) <- names(families)
-    
+
     mods <- 1:length(fit)
-    
+
     offset <- vector("list", length = length(mods))
     names(offset) <- names(families)
     for (j in mods){
@@ -170,20 +170,20 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                                       control=control, weights = w,
                                       ...))
     }
-    
+
     iBoost <- function(niter, method) {
-        
+
         start <- sapply(fit, mstop)
         # initialize combined_risk
         combined_risk <- NA
-        
+
         if (method == "noncyclic") {
             ### noncyclical fitting ###
             # this is the case for boosting from the beginning
             if (is.null(attr(fit, "combined_risk")) | niter == 0) {
                 combined_risk <- vapply(fit, risk, numeric(1))
             }
-            
+
             best <- which(names(fit) == tail(names(combined_risk), 1))
         } else {
             ### cyclical fitting ###
@@ -194,20 +194,20 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                     mvals[[j]][1:niter[j]] <- (start[j] + 1):(start[j] + niter[j])
             }
         }
-        
+
         ENV <- lapply(mods, function(j) environment(fit[[j]]$subset))
         ## main loop starts here ##
         for (i in 1:max(niter)){
             if (method == "noncyclic") {
                 ### noncyclical fitting ###
-                
+
                 ## update value of nuisance parameters
                 ## use response(fitted()) as this is much quicker than fitted(, type = response)
                 for( k in mods[-best]) {
-                    assign(names(fit)[best], families[[best]]@response(fitted(fit[[best]])), 
+                    assign(names(fit)[best], families[[best]]@response(fitted(fit[[best]])),
                            environment(get("ngradient", environment(fit[[k]]$subset))))
                 }
-                
+
                 risks <- numeric(length(fit))
                 for(b  in 1:length(fit)){
                     st <- mstop(fit[[b]])
@@ -216,9 +216,9 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                     #evalq({riskfct(y, fit, weights)}, envir = ENV[[b]])
                     #risks[b] <- ENV[[b]][["riskfct"]](ENV[[b]][["y"]], ENV[[b]][["fit"]], ENV[[b]][["weights"]])
                     fit[[b]][st]
-                    
+
                     ## fit[[b]][st] is not enough to reduce the model back to beginning, so
-                    ## so all these values have to be reduced, so that they are calculated 
+                    ## so all these values have to be reduced, so that they are calculated
                     ## correctly the next time
                     evalq({
                         xselect <- xselect[seq_len(mstop)];
@@ -228,23 +228,23 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                     },
                     environment(fit[[b]]$subset))
                 }
-                
+
                 best <- which.min(risks)
-                
+
                 ## update value of u, i.e. compute ngradient with new nuisance parameters
                 evalq({u <- ngradient(y, fit, weights)}, ENV[[best]])
                 #ENV[[best]][["u"]] <- ENV[[best]][["ngradient"]](ENV[[best]][["y"]], ENV[[best]][["fit"]], ENV[[best]][["weights"]])
-                
+
                 ## update selected component by 1
                 fit[[best]][mstop(fit[[best]]) + 1]
-                
+
                 ## update risk list
                 combined_risk[(length(combined_risk) + 1)] <- tail(risk(fit[[best]]), 1)
                 names(combined_risk)[length(combined_risk)] <- names(fit)[best]
                 combined_risk <<- combined_risk
-                
+
             } else {
-                ### cyclical fitting ### 
+                ### cyclical fitting ###
                 for (j in mods){
                     ## update value of nuisance parameters
                     ## use response(fitted()) as this is much quicker than fitted(, type = response)
@@ -252,20 +252,20 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                         assign(names(fit)[k], families[[k]]@response(fitted(fit[[k]])),
                                environment(get("ngradient", environment(fit[[j]]$subset))))
                     ## update value of u, i.e. compute ngradient with new nuisance parameters
-                    
+
                     ENV[[j]][["u"]] <- ENV[[j]][["ngradient"]](ENV[[j]][["y"]], ENV[[j]][["fit"]],
                                                                ENV[[j]][["weights"]])
                     # same as:
                     # evalq(u <- ngradient(y, fit, weights), environment(fit[[j]]$subset))
-                    
+
                     ## update j-th component to "m-th" boosting step
                     fit[[j]][mvals[[j]][i]]
                 }
             }
-            
+
             if (trace){
                 if (method == "noncyclic") {
-                    do_trace(current = length(combined_risk) - length(fit), mstart = sum(start), 
+                    do_trace(current = length(combined_risk) - length(fit), mstart = sum(start),
                              risk = combined_risk[-(1:length(fit))], mstop = niter)
                 } else {
                     ## which is the current risk? rev() needed to get the last
@@ -276,20 +276,20 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                              mstop = max(niter),
                              risk = risk(fit[[whichRisk]])[-1])
                 }
-                
+
             }
         }
         return(TRUE)
     }
-    
+
     if (any(mstop > 0))
         tmp <- iBoost(mstop, method = method)
-    
+
     class(fit) <- c(paste0(funchar, "LSS"), "mboostLSS")
     if(method != "cyclic"){
         class(fit) <- c("nc_mboostLSS", class(fit))
     }
-    
+
     ### update to a new number of boosting iterations mstop
     ### i <= mstop means less iterations than current
     ### i >  mstop needs additional computations
@@ -297,9 +297,9 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
     ### some models are CHANGED!
     if(method == "cyclic") {
         attr(fit, "subset") <- function(i) {
-            
+
             i <- check(i, "mstop", names(families))
-            
+
             msf <- mstop(fit)
             niter <- i - msf
             if (all(niter == 0)) {
@@ -308,22 +308,22 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
             } else {
                 minStart <- min(msf[niter != 0], i[niter != 0])
             }
-            
+
             ## check if minStart bigger than mstop of parameters that are not touched
             #if (length(msf[niter == 0]) > 0 && minStart < min(msf[niter == 0]))
             #minStart <- min(msf[niter == 0])
-            
+
             ## reduce models first (when necessary)
             if (any(msf > minStart)){
                 cf <- class(fit)
                 class(fit) <- "list" ## needed to use [] operator for lists
-                
+
                 #cat("processed parameters: ", paste(names(fit[msf > minStart]),
                 #                                    collapse = ", "), "\n")
-                
+
                 lapply(fit[msf > minStart],
                        function(obj) obj$subset(minStart))
-                
+
                 ## remove additional boosting iterations from environments
                 lapply(fit[msf > minStart], function(obj){
                     evalq({xselect <- xselect[seq_len(mstop)];
@@ -332,13 +332,13 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                     nuisance <- nuisance[seq_len(mstop)]},
                     environment(obj$subset))
                 })
-                
+
                 class(fit) <- cf
                 if (trace)
                     cat("Model first reduced to mstop = ", minStart, ".\n",
                         "Now continue ...\n", sep ="")
             }
-            
+
             ## now increase models (when necessary)
             if (any(i > minStart)){
                 ## set negative values to 0
@@ -346,37 +346,36 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                 inc <- ifelse(i - minStart > 0, i - minStart, 0)
                 tmp <- iBoost(inc, method = method)
             }
-            
+
             mstop <<- i
         }
-    } 
+    }
     else {
         attr(fit, "subset") <- function(i) {
             msf <- sum(mstop(fit))
             niter <- i - msf
-            
+
             ## check if minStart bigger than mstop of parameters that are not touched
             #if (length(msf[niter == 0]) > 0 && minStart < min(msf[niter == 0]))
             #minStart <- min(msf[niter == 0])
-            
+
             ## reduce models first (when necessary)
             if (niter < 0 ){
                 cf <- class(fit)
                 d = length(fit)
                 class(fit) <- "list" ## needed to use [] operator for lists
-                
                 #cat("processed parameters: ", paste(names(fit[msf > minStart]),
                 #                                    collapse = ", "), "\n")
-                
                 #reduce the combined risk values
+                combined_risk <- attr(fit, "combined_risk")()
                 combined_risk <<- attr(fit, "combined_risk")()[seq_len(i + d)]
                 new_stop_value <- table(names(attr(fit, "combined_risk")())) - 1
-                
-                
+
+
                 for(o in names(new_stop_value)){
                     fit[[o]]$subset(new_stop_value[o])
                 }
-                
+
                 ## remove additional boosting iterations from environments
                 lapply(fit, function(obj){
                     evalq({xselect <- xselect[seq_len(mstop)];
@@ -385,7 +384,7 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                     nuisance <- nuisance[seq_len(mstop)]},
                     environment(obj$subset))
                 })
-                
+
                 #update ALL nuisance parameters to last update
                 ENV <- lapply(mods, function(j) environment(fit[[j]]$subset))
                 for(j in names(new_stop_value)){
@@ -394,24 +393,24 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
                                environment(get("ngradient", environment(fit[[j]]$subset))))
                     }
                 }
-                
+
                 for(k in mods){
                     evalq(u <- get("ngradient")(get("y"), fit, weights), ENV[[k]])
                 }
-                
-                
+
+
                 class(fit) <- cf
             }
-            
+
             ## now increase models (when necessary)
             else if (niter > 0){
                 tmp <- iBoost(niter, method = method)
             }
-            
+
             mstop <<- i
         }
     }
-    
+
     ## make call in submodels nicer:
     cl <- call
     cl[[1]] <- as.name(gsub("LSS", "", cl[[1]]))
@@ -421,9 +420,9 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
         ## <FIXME> This is not really nice
         fit[[i]]$call$family <- families[[i]]
     }
-    
+
     attr(fit, "(weights)") <- weights  ## attach weights used for fitting
-    
+
     ## update to new weights; just a fresh start
     attr(fit, "update") <- function(weights = NULL, oobweights = NULL,
                                     risk = NULL, trace = NULL, mstop = NULL) {
@@ -451,6 +450,6 @@ mboostLSS_fit <- function(formula, data = list(), families = GaussianLSS(),
     attr(fit, "families") <- families
     if(method != "cyclic")
         attr(fit, "combined_risk") <- function() combined_risk
-    
+
     return(fit)
 }
