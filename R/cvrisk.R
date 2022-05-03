6cf9cb8e2f3a5c6861c10cef443013b3b99eea25
Benjamin Hofner <benjamin.hofner@pei.de>
2022-05-03 13:27:46 +0200

    Update cvrisk.R
    Fix issue with all.equal(papply, mclapply) == TRUE which was not valid syntax

--- R/cvrisk.R
+++ R/cvrisk.R
@@ -191,7 +191,7 @@ cvrisk.mboostLSS <- function(object, folds = cv(model.weights(object)),
     
     OOBweights <- matrix(rep(weights, ncol(folds)), ncol = ncol(folds))
     OOBweights[folds > 0] <- 0
-    if (all.equal(papply, mclapply) == TRUE) {
+    if (identical(papply, mclapply)) {
         oobrisk <- papply(1:ncol(folds),
                           function(i) dummyfct(i, weights = folds[, i],
                                                oobweights = OOBweights[, i]),
