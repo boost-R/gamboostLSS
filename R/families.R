24635504c117156292563ea342e6b9b0ec8677b7
unknown <michael.balzer@tutanota.com>
2024-12-25 16:33:19 +0100

    implement Dirichlet family

--- R/families.R
+++ R/families.R
@@ -880,3 +880,67 @@ ZINBLSS <- function(mu = NULL, sigma = NULL, nu = NULL,
     
     fam
 }
+
+###
+# Dirichtlet LSS Family
+DirichletAlpha = function(k=k,
+                          a1=NULL,a2=NULL,a3=NULL,a4=NULL,a5=NULL,a6=NULL,a7=NULL,a8=NULL,a9=NULL,
+                          K=K) {
+
+  arg <- c("y",paste0("a",1:K))
+  arg[arg %in% paste0("a",k)] <- "f"
+  
+  # Loss function is the negative log-likelihood of Dirichlet distribution
+  eval(parse(text=paste0("loss = function(",paste(arg,collapse  = ","),"){
+       A =cbind(",gsub("f","exp(f)",paste0(grep("a|f",arg,value=TRUE),collapse=",")),")
+       return ( - ( lgamma(rowSums(A)) - rowSums(lgamma(A)) + rowSums((A-1)*log(y)) ))}")))
+  
+  # Negative gradient is the first partial derivative of the NLL with respect to all distributional parameters
+  
+  eval(parse(text=paste0("ngradient = function(y,f,w=1){
+       A =cbind(",gsub("f","exp(f)",paste0(grep("a|f",arg,value=TRUE),collapse=",")),")
+
+       ngr = A[,k] * (digamma(rowSums(A)) - digamma(A[,k]) + log(y[,k]))
+
+       return(ngr)}")))
+  
+  
+  # Risk is sum of the loss
+  eval(parse(text=paste0("risk = function(y,f,w=1){
+       sum(w * loss(",paste(arg,arg,sep = "=",collapse=","),"))}")))
+  
+  # Choose offset values 
+  offset <- function(y, w) {
+    
+    if (!is.null(get(paste0("a",k)))) {
+      RET <- log(get(paste0("a",k)))
+    }
+    else {
+      RET <- min(y[,k])
+    }
+    return(RET)
+  }
+  
+  Family(ngradient = ngradient, risk = risk, loss = loss, response =
+           function(f) exp(f), offset = offset, name = paste0("Dirichlet Distribution: a",k,"(log link)"))
+}
+
+DirichletLSS  <- function(K,a1=NULL,a2=NULL,a3=NULL,a4=NULL,a5=NULL,a6=NULL,a7=NULL,a8=NULL,a9=NULL,
+           
+           m1=NULL,m2=NULL,m3=NULL,m4=NULL,m5=NULL,m6=NULL,m7=NULL,m8=NULL,m9=NULL,
+           
+           s1=NULL,s2=NULL,s3=NULL,s4=NULL,s5=NULL,s6=NULL,s7=NULL,s8=NULL,s9=NULL,
+           ...) {
+    
+    pars <- paste(paste0("a",1:K),collapse = ",")
+    
+    fam <- paste(sapply(1:K,function(k){
+      paste0("a",k,"=DirichletAlpha(k=",k,",",pars,",K=K)")
+    }),collapse=",")
+    
+    
+    eval(parse(text=paste0("Families(",paste(fam,sep=","),")")))
+    
+    
+  }
+
