aabdab6fccd74e86db23f36f5a7b2e2787b07376
Bjarne Herbst <bjarne.herbst01@stud.uni-goettingen.de>
2026-06-10 11:07:57 +0200

    test(copula): add test for discrete_discrete_loglik

--- tests/regtest-copula.R
+++ tests/regtest-copula.R
@@ -17,6 +17,37 @@ stopifnot(max(abs(gamboostLSS:::continuous_continuous_loglik(y,
                                                          list(mu=0, sigma=1)
                                                          )))) < 1e-6)
 
+### check discrete_discrete_loglik() accuracy at theta = 0
+theta <- 0
+y <- cbind(c(0, 1, 2, 5), c(0, 2, 3, 1))
+
+stopifnot(max(abs(gamboostLSS:::discrete_discrete_loglik(y, 
+                                                             NBinomialLSS(), 
+                                                             NBinomialLSS(), 
+                                                             list(mu=2, sigma=1), 
+                                                             list(mu=2, sigma=1), 
+                                                             "gaussian", theta) -
+                    (gamboostLSS:::get_marginal_logpdf(NBinomialLSS(),
+                                                       y[,1],
+                                                       list(mu=2, sigma=1)) +
+                       gamboostLSS:::get_marginal_logpdf(NBinomialLSS(),
+                                                         y[,2],
+                                                         list(mu=2, sigma=1)
+                       )))) < 1e-3) # lower accuracy threshold due to accuracy 
+                                    # of pmvnorm
+
+### check discrete_discrete_loglik() accuracy for large theta and extreme 
+### combination of response
+theta <- 0.95
+y <- cbind(0, 20)
+stopifnot(is.finite(gamboostLSS:::discrete_discrete_loglik(y, 
+                                                           NBinomialLSS(), 
+                                                           NBinomialLSS(), 
+                                                           list(mu=2, sigma=1), 
+                                                           list(mu=2, sigma=1), 
+                                                           "gaussian", theta)))
+
+### numerical gradient check
 cop <- gamboostLSS:::get_copula("gaussian")
 test_points <- list(
   # standard case - all params non-problematic
@@ -30,7 +61,7 @@ test_points <- list(
 )
 eps <- 1e-6
 
-### check analytical against numerical gradient of gaussian-copula (u1)
+# check analytical against numerical gradient of gaussian-copula (u1)
 for (p in test_points){
   num_grad_u1 <- (cop$logdcopula(p$u1 + eps, p$u2, p$theta) - 
                     cop$logdcopula(p$u1 - eps, p$u2, p$theta)) / (2*eps)
@@ -39,7 +70,7 @@ for (p in test_points){
   stopifnot(abs(num_grad_u1 - ana_grad_u1) < 1e-5)
 }
 
-### check analytical against numerical gradient of gaussian-copula (theta)
+# check analytical against numerical gradient of gaussian-copula (theta)
 for (p in test_points){
   num_grad_theta <- (cop$logdcopula(p$u1, p$u2, p$theta + eps) -
                        cop$logdcopula(p$u1, p$u2, p$theta - eps)) / (2*eps)
